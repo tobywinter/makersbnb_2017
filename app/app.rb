e815cd3f6d7e97b9129cbb4ac798d245c923eb81
Toby Winter <toby.hw@live.co.uk>
2017-06-14 12:03:33 +0100

    Resolves problem cause by two conflicting controllers not caught in code review

--- app/app.rb
+++ app/app.rb
@@ -19,26 +19,26 @@ class MakersBnB < Sinatra::Base
     end
   end
 
-  get '/' do
-    current_user
-    erb :index
-  end
-
-  get '/users/new' do
-    @user = User.new
-    erb :'users/index'
-  end
-
-  post '/users' do
-    @user = User.create(email: params[:email],
-                        password: params[:password],
-                        password_confirmation: params[:password_confirmation])
-    if @user.save
-      session[:user_id] = @user.id
-      redirect '/'
-    else
-      flash.now[:errors] = @user.errors.full_messages
-      erb :'users/index'
-    end
-  end
+  # get '/' do
+  #   current_user
+  #   erb :index
+  # end
+  #
+  # get '/users/new' do
+  #   @user = User.new
+  #   erb :'users/index'
+  # end
+  #
+  # post '/users' do
+  #   @user = User.create(email: params[:email],
+  #                       password: params[:password],
+  #                       password_confirmation: params[:password_confirmation])
+  #   if @user.save
+  #     session[:user_id] = @user.id
+  #     redirect '/'
+  #   else
+  #     flash.now[:errors] = @user.errors.full_messages
+  #     erb :'users/index'
+  #   end
+  # end
 end
--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -6,15 +6,20 @@ class MakersBnB < Sinatra::Base
   end
 
   get '/users/new' do
+    @user = User.new
     erb :'users/index'
   end
 
   post '/users' do
-    @user = User.create(email: params[:email], password: params[:password])
+    @user = User.create(email: params[:email],
+                        password: params[:password],
+                        password_confirmation: params[:password_confirmation])
     if @user.save
       session[:user_id] = @user.id
       redirect '/'
+    else
+      flash.now[:errors] = @user.errors.full_messages
+      erb :'users/index'
     end
   end
-    
 end
