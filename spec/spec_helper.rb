5419b99d4bf980e86716b3d13c87a84bc07a5ba1
Dom <dom.mcdonnell@gmail.com>
2017-06-14 13:37:58 +0100

    User can see details for a given space

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,10 +2,13 @@ ENV['RACK_ENV'] = 'test'
 
 require 'capybara'
 require 'capybara/rspec'
+require 'database_cleaner'
 require 'rspec'
 require 'simplecov'
 require 'simplecov-console'
 require './app/app'
+
+require 'helpers/spaces'
 
 Capybara.app = MakersBnB
 
@@ -18,5 +21,24 @@ SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
 SimpleCov.start
 
 RSpec.configure do |config|
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+  # Everything in this block runs once before each individual test
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+  config.include SpaceHelpers
+  # Everything in this block runs once after each individual test
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+  config.after(:suite) do
+    puts
+    puts "\e[33mHave you considered running rubocop? It will help you improve your code!\e[0m"
+    puts "\e[33mTry it now! Just run: rubocop\e[0m"
+  end
+  config.include Capybara::DSL # good
 
 end
