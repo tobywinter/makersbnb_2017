662e4a20b16217c4049b7d5843a7f658bb662768
Lubos Michalic <lnmichalic@gmail.com>
2017-06-14 16:40:53 +0100

    Replace deleted Sign Out Test

---
+++ spec/features/signout_spec.rb
@@ -0,0 +1,14 @@
+feature 'Sign Out' do
+  before(:each) do
+    User.create(email: 'user@test.com',
+    password: 'password',
+    password_confirmation: 'password')
+  end
+
+  scenario 'while being signed in' do
+    sign_in(email: 'user@test.com', password: 'password')
+    click_button 'Sign Out'
+    expect(page).to have_content('Ciao!!')
+    expect(page).not_to have_content('Welcome, user@test.com')
+  end
+end
