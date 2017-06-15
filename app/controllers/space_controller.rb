d8512a29bfd967aaebc6456e11252210a29c06cc
Dom McDonnell <dom.mcdonnell@gmail.com>
2017-06-15 11:30:04 +0100

    Closes#9 (Feature 6: update space) (#17)
    * Added feature test and updated space controller/view to allow update of a space.
    
    * Fixed the return after update space back to space/:id.
    
    * Fixed the space update errors, now it is saving changes to database.

--- app/controllers/space_controller.rb
+++ app/controllers/space_controller.rb
@@ -2,7 +2,7 @@ class MakersBnB < Sinatra::Base
 
   get '/spaces' do
     @spaces = Space.all
-    erb :'spaces/index'
+    erb :'/spaces/index'
   end
 
   post '/spaces' do
@@ -17,18 +17,41 @@ class MakersBnB < Sinatra::Base
   end
 
   get '/spaces/new' do
-    erb :'spaces/new'
+    erb :'/spaces/new'
   end
 
   get '/spaces/:id' do
     @space = Space.first(id: params[:id])
-    # @space = space ? space.name : []
-    erb :'spaces/space'
+    session[:id] = params[:id]
+    erb :'/spaces/space'
+  end
+  
+  get '/spaces/:id/update' do
+    @space = Space.get(session[:id])
+    erb :'/spaces/update'
   end
 
-  get '/spaces/:id/update' do
-    space = Space.first(id: params[:id])
-    erb :'spaces/update'
+  post '/spaces/:id/update' do
+    session[:name] = params[:name]
+    session[:description] = params[:description]
+    session[:rate] = params[:rate]
+    session[:max_capacity] = params[:max_capacity]
+    session[:available_from_date] = params[:available_from_date]
+    session[:available_to_date] = params[:available_to_date]
+
+    @id = session[:id].to_i
+   
+    @space = Space.get(@id)
+    @space.update(:name => session[:name],
+                          :description => session[:description],
+                          :rate => session[:rate].to_i,
+                          :max_capacity => session[:max_capacity].to_i,
+                          :available_from_date => session[:available_from_date],
+                          :available_to_date => session[:available_to_date])
+    if @space.save
+      p 'saved after update'
+    end
+    redirect to "/spaces/#{@id}"
   end
 
 end
