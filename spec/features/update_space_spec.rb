b3d1af62bda270aecba6916144c79c62333df8cc
Dom McDonnell <dom.mcdonnell@gmail.com>
2017-06-15 15:15:12 +0100

    closes #18 adding return button (#23)
    * Adding feature test for the space nagigation
    
    * Adding return button from the view space and the missing add space button on the main page.
    
    * Minor change in index.erb.

--- spec/features/update_space_spec.rb
+++ spec/features/update_space_spec.rb
@@ -3,34 +3,34 @@ feature 'update space' do
     sign_up
     sign_in
     visit('/spaces/new')
-    fill_in('name', with: 'One')
+    fill_in('name', with: 'OneUpdate')
     enter_generic_details
 
     within 'ul#spaces' do
-      expect(page).to have_content('One')
+      expect(page).to have_content('OneUpdate')
     end
     visit('/spaces/new')
-    fill_in('name', with: 'Two')
+    fill_in('name', with: 'TwoUpdate')
     enter_generic_details
 
     within 'ul#spaces' do
-      expect(page).to have_content('One')
-      expect(page).to have_content('Two')
+      expect(page).to have_content('OneUpdate')
+      expect(page).to have_content('TwoUpdate')
     end
 
     first(:link, 'See Space').click
 
-    expect(current_path).to eq('/spaces/4')
-    expect(page).to have_content('One')
+    expect(current_path).to eq('/spaces/6')
+    expect(page).to have_content('OneUpdate')
 
     click_button('Edit Space')
-    expect(current_path).to eq('/spaces/4/update')
+    expect(current_path).to eq('/spaces/6/update')
 
     fill_in('description', with: 'Shoe Box')
     fill_in('rate', with: 100)
 
     click_button('Update my Space')
-    expect(current_path).to eq('/spaces/4')
+    expect(current_path).to eq('/spaces/6')
     expect(page).to have_content('Shoe Box')
 
   end
